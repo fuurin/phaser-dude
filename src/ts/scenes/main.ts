a172959d776d748791c8743d2ce503fa63ef5ce7
fuurin <komatsuemgbr0624@gmail.com>
2021-02-25 00:19:48 +0900

    part 5 & part 6

--- src/ts/scenes/main.ts
+++ src/ts/scenes/main.ts
@@ -19,7 +19,9 @@ export default class Main extends Phaser.Scene {
 
   create(): void {
     this.createSky()
-    this.createPlatform()
+    const platforms = this.createPlatform()
+    const player = this.createPlayer()
+    this.physics.add.collider(player, platforms)
   }
 
   update(): void {
@@ -39,5 +41,38 @@ export default class Main extends Phaser.Scene {
     platforms.create(750, 220, 'platform')
 
     return platforms
+  }
+
+  private createPlayer(): Phaser.Types.Physics.Arcade.SpriteWithDynamicBody {
+    const player = this.physics.add.sprite(100, 450, 'dude')
+    player.setBounce(0.2)
+    player.setCollideWorldBounds(true)
+    player.body.setGravityY(300)
+    this.createPlayerAnimations()
+    return player
+  }
+
+  private createPlayerAnimations(): Phaser.Animations.AnimationManager {
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20
+    })
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    return this.anims
   }
 }
