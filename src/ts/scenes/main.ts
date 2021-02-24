f847023be587e42e0bf71ce92e3c97456f75f356
fuurin <komatsuemgbr0624@gmail.com>
2021-02-24 23:24:28 +0900

    part 3 & part 4

--- src/ts/scenes/main.ts
+++ src/ts/scenes/main.ts
@@ -18,10 +18,26 @@ export default class Main extends Phaser.Scene {
   }
 
   create(): void {
-    this.add.image(400, 300, 'sky')
+    this.createSky()
+    this.createPlatform()
   }
 
   update(): void {
     return
+  }
+
+  private createSky(): Phaser.GameObjects.Image {
+    return this.add.image(400, 300, 'sky')
+  }
+
+  private createPlatform(): Phaser.Physics.Arcade.StaticGroup {
+    const platforms = this.physics.add.staticGroup()
+
+    platforms.create(400, 568, 'platform').setScale(2).refreshBody()
+    platforms.create(600, 400, 'platform')
+    platforms.create(50, 250, 'platform')
+    platforms.create(750, 220, 'platform')
+
+    return platforms
   }
 }
